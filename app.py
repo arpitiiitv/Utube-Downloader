e0dce45adec2a3070b66fc0395ed7eed607e553d
Arpit tiwari <38579437+arpitiiitv@users.noreply.github.com>
2020-08-18 13:58:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template , request
+import pafy
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/download', methods=['POST'])
+def download():
+
+    try:
+        url = request.form["videolink"]
+        type = request.form["downloadtype"]
+        filename = pafy.new(url).getbest(preftype='mp4').download(quiet=False)
+        return render_template('index.html', url=url, type=type)
+
+    except Exception as e:
+        print(e)
+        return "Please try again"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
